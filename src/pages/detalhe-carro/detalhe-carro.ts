3ffca1834977f8446d232380195185e922ac17e1
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-30 18:25:45 -0300

    Calculando o preço total

--- src/pages/detalhe-carro/detalhe-carro.ts
+++ src/pages/detalhe-carro/detalhe-carro.ts
@@ -1,24 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { Carro } from '../../models/carro';
+import { Acessorio } from '../../models/acessorio';
 
 @IonicPage()
 @Component({
   selector: 'page-detalhe-carro',
   templateUrl: 'detalhe-carro.html',
 })
-export class DetalheCarroPage {
-
+export class DetalheCarroPage implements OnInit{
+  
   carro: Carro;
-  acessorios = [];
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  acessorios: Acessorio[];
+  private _precoTotal: number;
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams) { }
+  
+  ngOnInit(): void {
     this.carro = this.navParams.get('carro');
+    this._precoTotal = this.carro.preco;
     this.acessorios = [
       { nome: 'Freio ABS', preco: 800 },
       { nome: 'Ar-Condicionado', preco: 1000 },
       { nome: 'MP3 Player', preco: 500 }
     ];
   }
+
+  get precoTotal() {
+    return this._precoTotal;
+  }
+
+  atualizaTotal(ativado: boolean, acessorio: Acessorio){
+    ativado ? this._precoTotal += acessorio.preco : this._precoTotal -= acessorio.preco;
+  }
+
 }
