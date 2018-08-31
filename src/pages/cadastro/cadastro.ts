cf0ec5490fcac6a629f5d3000d49a709917599bc
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-31 11:26:03 -0300

    Agendando o test drive

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -3,20 +3,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FormGroup, FormBuilder } from '@angular/forms';
 
 import { Carro } from '../../models/carro';
+import { AgendamentosServiceProvider } from '../../providers/agendamentos/agendamentos.service';
 
 @IonicPage()
 @Component({
   selector: 'page-cadastro',
   templateUrl: 'cadastro.html',
 })
-export class CadastroPage implements OnInit{
-  
+export class CadastroPage implements OnInit {
+
   carro: Carro;
   precoTotal: number;
 
   cadastroForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder, private agendamentoService: AgendamentosServiceProvider) {
     this.cadastroForm = this.formBuilder.group({
       nome: [''],
       endereco: [''],
@@ -24,14 +25,28 @@ export class CadastroPage implements OnInit{
       data: ['']
     });
   }
-  
+
   ngOnInit(): void {
     this.carro = this.navParams.get('carro');
     this.precoTotal = this.navParams.get('precoTotal');
   }
 
   agenda() {
-    console.log('Agendou');
-    console.log(this.cadastroForm.value);
+    let agendamento = {
+      nomeCliente: this.cadastroForm.get('nome').value,
+      enderecoCliente: this.cadastroForm.get('endereco').value,
+      emailCliente: this.cadastroForm.get('email').value,
+      modeloCarro: this.carro.nome,
+      precoTotal: this.precoTotal
+    };
+    
+    console.log(agendamento);
+    
+    this.agendamentoService
+      .agenda(agendamento)
+      .subscribe(
+        () => alert('Agendou!'),
+        () => alert('Deu ruim!')
+      );
   }
 }
