8e3579be3a07bbaa5ff73f39a3cd798f93e8ee8a
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-30 22:53:01 -0300

    Criando o formulário de cadastro

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Carro } from '../../models/carro';
 
 @IonicPage()
 @Component({
   selector: 'page-cadastro',
   templateUrl: 'cadastro.html',
 })
-export class CadastroPage {
+export class CadastroPage implements OnInit{
+  
+  carro: Carro;
+  precoTotal: number;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) { }
+  
+  ngOnInit(): void {
+    this.carro = this.navParams.get('carro');
+    this.precoTotal = this.navParams.get('precoTotal');
+  }
 }
