8450d67f89b65ad1d9814a92d46280f161227d61
vinicius-rocha <r.vinicius.09@gmail.com>
2018-09-04 14:15:30 -0300

    Retornando a página principal - HomePage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { ListaAgendamentosPage } from '../pages/lista-agendamentos/lista-agendamentos';
+import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = ListaAgendamentosPage.name;
+  rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
