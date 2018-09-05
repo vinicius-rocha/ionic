2fdc54724ac6a4f590d2c887bd1fbcf9c3c79d2a
vinicius-rocha <r.vinicius.09@gmail.com>
2018-09-05 18:44:07 -0300

    Configurando o OneSignal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { OneSignal, OSNotification } from '@ionic-native/onesignal';
 
 import { ListaAgendamentosPage } from '../pages/lista-agendamentos/lista-agendamentos';
 import { LoginPage } from '../pages/login/login';
@@ -22,19 +23,37 @@ export class MyApp {
     { titulo: 'Perfil', componente: PerfilPage.name, icone: 'person' },
     { titulo: 'Agendamentos', componente: ListaAgendamentosPage.name, icone: 'calendar' }
   ];
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private usuarioService: UsuariosServiceProvider) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private usuarioService: UsuariosServiceProvider, private oneSignal: OneSignal) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      //config onesignal
+      this.configuraPushNotification();
     });
+  }
+
+  private configuraPushNotification() {
+    let iOSconfig = {
+      kOSSettingsKeyAutoPrompot: true,
+      kOSSettingsKeyInAppLaunchURL: false
+    };
+    this.oneSignal
+      .startInit('eec780a3-a908-454d-bd6f-00de644c7a6e', '419962267366')
+      .iOSconfig(iOSconfig);
+    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.Notification);
+    this.oneSignal.handleNotificationReceived()
+      .subscribe((notificacao: OSNotification) => {
+      });
+    this.oneSignal.endInit();
   }
 
   irParaPagina(componente) {
     this.nav.push(componente);
   }
-  
+
   get usuarioLogado() {
     return this.usuarioService.usuarioLogado;
   }
