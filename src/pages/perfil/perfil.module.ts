3cb3af8f353d4ac51e14587e817ff8931bc76439
vinicius-rocha <r.vinicius.09@gmail.com>
2018-09-04 21:38:23 -0300

    Criando o component/page Perfil

---
+++ src/pages/perfil/perfil.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+
+import { PerfilPage } from './perfil';
+
+@NgModule({
+  declarations: [
+    PerfilPage
+  ],
+  imports: [
+    IonicPageModule.forChild(PerfilPage)
+  ],
+  exports: [
+    PerfilPage
+  ]
+})
+export class PerfilPageModule { }
